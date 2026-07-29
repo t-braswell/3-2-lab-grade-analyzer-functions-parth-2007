f98e12be7b8992ef1294abd4460e7aa6c3caae09
tara <tara.braswell@yahoo.com>
2026-07-29 14:29:49 -0700

    turned if-else into repeated ifs (doenst do anything; but all code was same as mine)

--- src/grade_analyzer.cpp
+++ src/grade_analyzer.cpp
@@ -17,11 +17,11 @@ double calculateAverage(double total, int count) {
 char determineLetterGrade(double average) {
     if(average >= 90){
         return 'A';
-    } else if (average >= 80){
+    }  if (average >= 80){
         return 'B';
-    } else if (average >= 70){
+    }  if (average >= 70){
         return 'C';
-    } else if (average >= 60){
+    }  if (average >= 60){
         return 'D';
     } else {
         return 'F';
